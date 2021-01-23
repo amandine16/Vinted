945c6b56839c9e7e023a63efb2f2831e8420cbd7
Amandine <amandinedelavoie@live.fr>
2021-01-23 16:32:09 +0100

    changed namet

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(formidable());
 app.use(cors());
 
-//Initialisation de la bdd avec nom Vinted
+//Initialisation de la bdd avec nom Vintede
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
