e972800f6deeb24bcefe2ebe543772490c3ce4d8
Amandine <amandinedelavoie@live.fr>
2021-02-12 11:42:30 +0100

    Changed comments and verify road login and signup

--- routes/user.js
+++ routes/user.js
@@ -12,17 +12,18 @@ router.post("/user/signup/", async (req, res) => {
   try {
     //Test si je reçois toutes les infos dont j'ai besoin
     if (req.fields.email && req.fields.username && req.fields.password) {
-      //Cryptage du password
+      //Etape 1 : Cryptage du password
       //Générer un salt
       const salt = uid2(64);
       //Générer un hash
       const hash = SHA256(req.fields.password + salt).toString(encBase64);
       //Génrer un token
       const token = uid2(64);
-      //Test si l'email existe déjà
+
+      //Etape 2 : Test si l'email existe déjà
       const user = await User.find({ email: req.fields.email });
       if (user.length <= 0) {
-        //Si pas de mail déjà présent => Création d'un nouveau user
+        // Etape 3 : Créer le nouvel utilisateur
         const newUser = new User({
           email: req.fields.email,
           account: {
@@ -33,9 +34,8 @@ router.post("/user/signup/", async (req, res) => {
           hash: hash,
           salt: salt,
         });
-        //Sauvegarde
+        //Etape 4 : Sauvegarder le nouveau utilisateur
         await newUser.save();
-        //Réponse
         res.status(200).json({
           _id: newUser._id,
           token: newUser.token,
@@ -50,11 +50,13 @@ router.post("/user/signup/", async (req, res) => {
         });
       }
     } else {
+      console.log(error.message);
       res.status(400).json({
         message: "Missing parameters",
       });
     }
   } catch (error) {
+    console.log(error.message);
     res.status(400).json({
       message: error.message,
     });
@@ -63,9 +65,10 @@ router.post("/user/signup/", async (req, res) => {
 
 router.post("/user/login/", async (req, res) => {
   try {
-    //Récupérer les infos du user via l'email
+    //Etape 1 : Récupérer les infos du user via l'email
     const user = await User.findOne({ email: req.fields.email });
     if (user) {
+      //Etape 2 : Tester s'il a envoyé le bon mot de passe
       //Récupérer le password
       const password = req.fields.password;
       //Générer un newHash
